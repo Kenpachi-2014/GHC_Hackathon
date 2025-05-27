5d1acba83f079bd33b3062bd57868d81a1830d03
Jørgen Eide <joergen.eide@nexigroup.com>
2025-05-27 11:25:41 +0200

    Apply classes in ExpenseTrackerApplicationTests

--- expense-tracker-src/expense-tracker-app/backend/src/test/java/com/example/expensetracker/ExpenseTrackerApplicationTests.java
+++ expense-tracker-src/expense-tracker-app/backend/src/test/java/com/example/expensetracker/ExpenseTrackerApplicationTests.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.example.expensetracker.ExpenseTrackerApplication.class)
 class ExpenseTrackerApplicationTests {
 
     @Test
