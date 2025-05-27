ec0d9a04f6c48110b2bda49b3e965060da8e89c3
Karthikeyan <clickforkarthik@gmail.com>
2025-05-27 18:57:03 +0200

    Delete expense_tracker/schema directory

--- expense_tracker/schema/expense_list.sql
+++
@@ -1,8 +0,0 @@
-USE expense_tracker;
-CREATE TABLE expense_list (
-    id SERIAL PRIMARY KEY,
-    expense_id BIGINT UNSIGNED NOT NULL,
-    category VARCHAR(255) NOT NULL,
-    notes TEXT,
-    FOREIGN KEY (expense_id) REFERENCES expense(id) ON DELETE CASCADE
-);
